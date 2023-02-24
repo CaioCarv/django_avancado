966a9a1b8c1b43c535b9ad65afce563cfad90f02
CaioCarv <caiocfvc@gmail.com>
2023-02-23 23:57:40 -0300

    up id

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<id>/', views.recipe),
+    path('', views.home, name="recipes-home"),
+    path('recipes/<id>/', views.recipe, name="recipes-recipe"),
 ]
