171451fbb8794b9fb4169f6b06dd4da867797c16
CaioCarv <caiocfvc@gmail.com>
2023-03-02 23:41:24 -0300

    Test

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,6 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
+from recipes import views
 
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        print('Hello World!')
+    def test_home_urls_is_correct(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_home_category_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_home_recipe_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(
+            reverse('recipes:category', kwargs={'category_id': 1})
+            )
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_recipe_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
