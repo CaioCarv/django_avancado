28ac1e487b1c310f74f02098cfd43895ac3450c5
CaioCarv <caiocfvc@gmail.com>
2023-03-07 22:54:39 -0300

    mais tests

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,6 +4,7 @@ from recipes import views
 from recipes.models import Category, Recipe, User
 
 
+
 class RecipeViewsTest(TestCase):
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(
@@ -28,7 +29,35 @@ class RecipeViewsTest(TestCase):
 
     def test_recipe_home_template_loads_recipes(self):
         category = Category.objects.create(name='Category')
-        assert 1 == 1 
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published='True',
+        )
+
+        response = self.client.get(reverse('recipes:home'))
+        response_recipes = response.context['recipes']
+
+        self.assertEqual(response_recipes.first().title, 'Recipe Title')
+
+        pass
 
     def test_recipe_category_view_function_is_correct(self):
         view = resolve(
